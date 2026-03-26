fedaf52407be7785a13b1e393f4328b913be9674
Varun <varuneshv05@gmail.com>
2026-03-26 15:01:05 +0530

    Added readme

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
